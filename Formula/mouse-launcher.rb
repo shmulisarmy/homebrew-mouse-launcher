e37911fd647ef27acfd05a5f3fe26a950c03aa56
shmulisamry <shmulikeller@gmail.com>
2025-07-05 22:59:07 -0400

    Fix formula URL to point to correct repository

--- Formula/mouse-launcher.rb
+++ Formula/mouse-launcher.rb
@@ -1,10 +1,10 @@
 class MouseLauncher < Formula
   desc "A simple mouse movement utility for macOS using arrow keys"
-  homepage "https://github.com/shmulisarmy/mouse-launcher"
+  homepage "https://github.com/shmulisarmy/homebrew-mouse-launcher"
   version "1.0.1"
   
   if OS.mac?
-    url "https://github.com/shmulisarmy/mouse-launcher/releases/download/v1.0.1/mouse_launcher_darwin_universal"
+    url "https://github.com/shmulisarmy/homebrew-mouse-launcher/releases/download/v1.0.1/mouse_launcher_darwin_universal"
     sha256 "PLACEHOLDER_SHA256" # Will be updated after release is created
   end
 
