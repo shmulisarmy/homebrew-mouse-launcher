b33d89b920bef0ff4112922357515807c49475db
shmulisamry <shmulikeller@gmail.com>
2025-07-05 22:51:40 -0400

    Update formula for v1.0.1

--- Formula/mouse-launcher.rb
+++ Formula/mouse-launcher.rb
@@ -1,20 +1,20 @@
 class MouseLauncher < Formula
   desc "A simple mouse movement utility for macOS using arrow keys"
   homepage "https://github.com/shmulisarmy/mouse-launcher"
-  version "1.0.0"
+  version "1.0.1"
   
   if OS.mac?
-    url "https://github.com/shmulisarmy/mouse-launcher/releases/download/v1.0.0/mouse_launcher_darwin_amd64"
-    sha256 "PLACEHOLDER_SHA256" # This will need to be updated after release
+    url "https://github.com/shmulisarmy/mouse-launcher/releases/download/v1.0.1/mouse_launcher_darwin_arm64"
+    sha256 "c77864cb1fec686e7e3a9aa60aebd1d123c8a7f03207e2a93f9cc018ded0ff14"
   end
 
   def install
     if OS.mac?
-      bin.install "mouse_launcher_darwin_amd64" => "mouse_launcher"
+      bin.install "mouse_launcher_darwin_arm64" => "mouse_launcher"
     end
   end
 
   test do
-    system "#{bin}/mouse_launcher", "--help"
+    system "#{bin}/mouse_launcher", "--version"
   end
 end 
